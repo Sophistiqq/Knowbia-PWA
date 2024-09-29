38832bb2ecd8d396e6bc30a2693e1312e0cf16b7
Roi Borromeo <roi.for.school@gmail.com>
2024-09-29 20:58:41 +0800

    Added Dynamic URL for connection

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import './assets/main.css'
+import '@fontsource/montserrat'
+import App from './App.svelte'
+import { setupInstallPrompt } from './scripts/install'
+
+
+const appElement = document.getElementById('app');
+if (!appElement) {
+  throw new Error("App element not found");
+}
+
+const app = new App({
+  target: appElement
+})
+
+setupInstallPrompt()
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, err => {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark')
+}
+
+export default app
---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte(), VitePWA({
+    strategies: 'injectManifest',
+    srcDir: 'src',
+    filename: 'sw.ts',
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+    manifest: {
+      name: 'Knowbia-PWA',
+      short_name: 'Knowbia-PWA',
+      description: 'Distribute',
+      display: 'standalone',
+      theme_color: '#ffffff',
+    },
+
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern'
+      }
+    }
+  }
+})
